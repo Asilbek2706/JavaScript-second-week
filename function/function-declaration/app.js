db034a1c4af97a9dfb33f8a3fb8ece86f296d7cd
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-28 23:58:55 +0500

    function and function-declaration

---
+++ function/function-declaration/app.js
@@ -0,0 +1,23 @@
+'use strict'
+function myFunction(num1, num2) {
+    return num1 + num2
+}
+
+console.log(myFunction(50, 40))
+
+function mySecondFunction(num1, num2) {
+    console.log(num1 * num2)
+}
+
+const result = mySecondFunction(6, 5)
+console.log(result)
+
+// ----------------------------------
+
+console.log(square(2))
+
+function square(num) {
+    return num * num
+}
+
+console.log(square(10))
