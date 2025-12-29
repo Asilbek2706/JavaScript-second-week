ae1d380a8bffe848cb2d0e40cf3a522d29941f78
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-29 22:42:25 +0500

    switch

---
+++ flow-control/switch.js
@@ -0,0 +1,15 @@
+const num = 2
+let output
+
+switch (num) {
+    case 1:
+        output = 'one'
+        break
+    case 2:
+        output = 'two'
+        break
+    default:
+        output = 'Not found'
+        break
+}
+console.log(`Value: ${output}`)
