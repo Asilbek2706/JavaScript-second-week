93fe0da217dada7df8d05265edc56aea64f4e37a
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-29 19:21:27 +0500

    Addition examples

--- array/array-methods/app.js
+++ array/array-methods/app.js
@@ -24,3 +24,33 @@ console.log(mappedValues)
 const sortedValues = (number1, number2) => number1 - number2
 result.sort(sortedValues)
 console.log(result)
+
+// const narxlar = [100, 250, 50, 400, 150]
+// const natija = narxlar.filter(narx => narx < 200).map(narx => narx * 1.1)
+// console.log(natija)
+
+const shaharlar = ["Samarqand", "Andijon", "Toshkent", "Buxoro"]
+const natija = shaharlar.sort()
+console.log(natija)
+
+const users = ["Ali", "Bobur", "Laylo", "Guli"]
+const natija1 = users.every(user => user.length > 2)
+console.log(natija1)
+
+
+// const numbers = [1, 3, 5, 8, 9, 11]
+// const natija2 = numbers.some(number => number % 2 === 0)
+// console.log(natija2)
+
+const numbers = [5, 12, 8, 130, 44]
+const natija2 = numbers.find(number => number > 10)
+console.log(natija2)
+
+const friends = ["Ali", "Vali", "G'ani", "Eshmat"]
+const natija3 = friends.findIndex(friend => friend === "G'ani")
+console.log(natija3)
+
+
+const words = ["olma", "anor", "behi", "shaftoli", "uzum"]
+const natija4 = words.find(word => word.length > 5)
+console.log(natija4)
