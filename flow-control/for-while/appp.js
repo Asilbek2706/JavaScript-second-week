07347a712dff71965f98f87aec137dea75cac792
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-29 23:18:17 +0500

    while, do-while

---
+++ flow-control/for-while/appp.js
@@ -0,0 +1,21 @@
+let i = 0
+while (i < 10) {
+    console.log("Hello World!")
+    i++
+}
+
+let a = 1, sum = 0
+while (a <= 10) {
+    sum += a
+    a++
+}
+console.log(sum)
+
+// do-while
+// do-while da kod bloki kamida bir marta ishga tushadi
+let b = 0
+do {
+    console.log("Hello Asilbek")
+    b++
+} while (b < 10)
+
