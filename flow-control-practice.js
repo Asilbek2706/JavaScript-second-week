3922dbe80b781940bf9dc5794572008d9097dc77
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-29 23:34:42 +0500

    Add simple calculator using switch case

--- flow-control-practice.js
+++ flow-control-practice.js
@@ -16,4 +16,25 @@ console.log(inRange(minRange, maxRange, givenNumber))
 
 
 // Brilgan ikki son ustida arifmetik amal bajarish
+const simpleCalculator = (num1, num2, oper) => {
+    switch (oper) {
+        case '+':
+            console.log(`Yig'indi: ${num1 + num2}`)
+            break
+        case '-':
+            console.log(`Ayirma: ${num1 - num2}`)
+            break
+        case '*':
+            console.log(`Ko'paytma: ${num1 * num2}`)
+            break
+        case '/':
+            console.log(`Bo'linma: $num1 / num2`)
+            break
+        default:
+            console.log('Invalid input')
+    }
+}
+
+console.log(simpleCalculator(5, 10, '*'))
+
 // Berilgan massivdagi eng katta qiymatga ega element
